1ae7b39cd3f727e2ab2123159ef0757022c7ea6d
JoaquinEscobar2002 <109183628+JoaquinEscobar2002@users.noreply.github.com>
2022-12-04 23:57:25 -0300

    Add files via upload

---
+++ js/Tercer_pre_entrega/js/stock.js
@@ -0,0 +1,10 @@
+let stockProductos = [
+    {id: 1, nombre: "Pizza 1", tipo: "Pizza", cantidad: 1, desc: "Una pizza pa", precio: 1200, img: './img/pizza.jpg'},
+    {id: 2, nombre: "Pizza 2", tipo: "Pizza", cantidad: 1, desc: "Una pizza pa", precio: 1100, img: './img/pizza.jpg'},
+    {id: 3, nombre: "Pizza 3", tipo: "Pizza", cantidad: 1, desc: "Una pizza pa", precio: 1200, img: './img/pizza.jpg'},
+    {id: 4, nombre: "hamburguesa 1", tipo: "Hamburguesa", cantidad: 1, desc: "Una hamburguesa pa", precio: 1400, img: './img/hamburg.jpg'},
+    {id: 5, nombre: "hamburguesa 2", tipo: "hamburguesa", cantidad: 1, desc: "Una hamburguesa pa", precio: 1200, img: './img/hamburg.jpg'},
+    {id: 6, nombre: "hamburguesa 3", tipo: "hamburguesa", cantidad: 1, desc: "Una hamburguesa pa", precio: 1500, img: './img/hamburg.jpg'},
+    {id: 7, nombre: "Pizza 4", tipo: "Pizza", cantidad: 1, desc: "Una pizza pa", precio: 1100, img: './img/pizza.jpg'},
+    {id: 8, nombre: "Pizza 5", tipo: "Pizza", cantidad: 1, desc: "Una pizza pa", precio: 1200, img: './img/pizza.jpg'}
+]
